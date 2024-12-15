0fef927b22ae3549d29b37a42c558a82635de29c
cr208 <cr.z2086@gmail.com>
2024-12-15 17:50:05 -0500

    Se corrige el model de dulceria

--- src/main/java/pe/edu/cibertec/apipeliculas/model/Dulceria.java
+++ src/main/java/pe/edu/cibertec/apipeliculas/model/Dulceria.java
@@ -5,7 +5,9 @@ import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "dulceria")
 public class Dulceria {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,5 +25,54 @@ public class Dulceria {
     private String descripcion;
 
     @Column
-    private String imagen; // URL de la imagen
+    private String imagen;
+
+    // Métodos Getters y Setters manuales
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
 }
